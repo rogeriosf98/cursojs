f93f306589287e74d78eaf5ebe47c575eed8a559
Rogério Ferreira <rogeriosfpro@gmail.com>
2026-04-26 07:09:07 -0300

    iniciando com condições

--- aula 11/ex008.js
+++ aula 11/ex008.js
@@ -1,6 +1,6 @@
 var vel = 68
 console.log(`A velocidade do seu carro é ${vel}Km/h`)
-if (vel > 60) {
+if (vel > 60) { // COndição simples!
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
 }
 console.log(`Dirija sempre usando o cinto de segurança`)
