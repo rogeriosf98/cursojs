e57aabc41350f304f7bff554c0c12c74727c868e
Rogério Ferreira <rogeriosfpro@gmail.com>
2026-04-26 13:51:17 -0300

    estudo de condições

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,20 +1,20 @@
 function carregar() {
-    var msg = document.getElementById('msg')
+    var msg = document.getElementById('msg') // guarda o elemento para poder manipular depois
     var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
-        // BOM DIA!
+        console.log('Bom dia!')
         img.src = 'fotomanha.jpg'
         document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora <= 18) {
-        // BOA TARDE!
+        console.log('Boa tia!')
         img.src = 'fototarde.jpg'
         document.body.style.background = '#b9846f'
     } else {
-        // BOA NOITE!
-        img.src = 'fotonoite.jpg'
+        console.log('Boa noite!')
+        img.s = 'fotonoite.jpg'
         document.body.style.background = '#515154'
     }
 }
