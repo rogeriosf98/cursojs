052bfebe0d1ec267bf1c52f8c2e2bbfd9e9128d9
Rogério Ferreira <rogeriosfpro@gmail.com>
2026-04-26 19:49:21 -0300

    p

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -5,16 +5,13 @@ function carregar() {
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
-        console.log('Bom dia!')
         img.src = 'fotomanha.jpg'
         document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora <= 18) {
-        console.log('Boa tia!')
         img.src = 'fototarde.jpg'
         document.body.style.background = '#b9846f'
     } else {
-        console.log('Boa noite!')
-        img.s = 'fotonoite.jpg'
+        img.src = 'fotonoite.jpg'
         document.body.style.background = '#515154'
     }
 }
